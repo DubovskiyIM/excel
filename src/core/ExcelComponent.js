f60f37103a9c2445b0d7b35002394ada75e4bc0f
IgnatDubovskiy <57371070+IgnatDubovskiy@users.noreply.github.com>
2020-05-18 12:23:06 +0300

    Finish base table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,15 +4,33 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || '';
+    this.observer = options.observer;
+    this.unsubscribers = [];
+
+    this.prepare();
   }
-  // Return component template
+
+  prepare() {}
+
   toHTML() {
     return '';
   }
+
+  $dispatch(event, ...args) {
+    this.observer.dispatch(event, ...args);
+  }
+
+  $on(event, callback) {
+    const unsubscribe = this.observer.subscribe(event, callback);
+    this.unsubscribers.push(unsubscribe);
+  }
+
   init() {
     this.initDOMListeners();
   }
+
   destroy() {
     this.removeDOMListeners();
+    this.unsubscribers.forEach(unsubscribe => unsubscribe());
   }
 }
